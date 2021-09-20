eb0461cfcf2f5791c088f576429ba78c559be46d
lichong0309 <chongli0309@gmail.com>
2021-09-20 20:25:07 +0900

    update

--- KDD_redundancy_free/han/model.py
+++ KDD_redundancy_free/han/model.py
@@ -1,17 +1,7 @@
-"""This model shows an example of using dgl.metapath_reachable_graph on the original heterogeneous
-graph.
-
-Because the original HAN implementation only gives the preprocessed homogeneous graph, this model
-could not reproduce the result in HAN as they did not provide the preprocessing code, and we
-constructed another dataset from ACM with a different set of papers, connections, features and
-labels.
-"""
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import dgl
 from dgl.nn.pytorch import GATConv
 
 class SemanticAttention(nn.Module):
@@ -37,7 +27,7 @@ class HANLayer(nn.Module):
 
     Arguments
     ---------
-    meta_paths : list of metapaths, each as a list of edge types
+    num_meta_paths : number of homogeneous graphs generated from the metapaths.
     in_size : input feature dimension
     out_size : output feature dimension
     layer_num_heads : number of attention heads
@@ -45,8 +35,8 @@ class HANLayer(nn.Module):
 
     Inputs
     ------
-    g : DGLHeteroGraph
-        The heterogeneous graph
+    g : list[DGLGraph]
+        List of graphs
     h : tensor
         Input features
 
@@ -55,46 +45,34 @@ class HANLayer(nn.Module):
     tensor
         The output feature
     """
-    def __init__(self, meta_paths, in_size, out_size, layer_num_heads, dropout):
+    def __init__(self, num_meta_paths, in_size, out_size, layer_num_heads, dropout):
         super(HANLayer, self).__init__()
 
         # One GAT layer for each meta path based adjacency matrix
         self.gat_layers = nn.ModuleList()
-        for i in range(len(meta_paths)):
+        for i in range(num_meta_paths):
             self.gat_layers.append(GATConv(in_size, out_size, layer_num_heads,
-                                           dropout, dropout, activation=F.elu,
-                                           allow_zero_in_degree=True))
+                                           dropout, dropout, activation=F.elu))
         self.semantic_attention = SemanticAttention(in_size=out_size * layer_num_heads)
-        self.meta_paths = list(tuple(meta_path) for meta_path in meta_paths)
+        self.num_meta_paths = num_meta_paths
 
-        self._cached_graph = None
-        self._cached_coalesced_graph = {}
-
-    def forward(self, g, h):
+    def forward(self, gs, h):
         semantic_embeddings = []
 
-        if self._cached_graph is None or self._cached_graph is not g:
-            self._cached_graph = g
-            self._cached_coalesced_graph.clear()
-            for meta_path in self.meta_paths:
-                self._cached_coalesced_graph[meta_path] = dgl.metapath_reachable_graph(
-                        g, meta_path)
-
-        for i, meta_path in enumerate(self.meta_paths):
-            new_g = self._cached_coalesced_graph[meta_path]
-            semantic_embeddings.append(self.gat_layers[i](new_g, h).flatten(1))
+        for i, g in enumerate(gs):
+            semantic_embeddings.append(self.gat_layers[i](g, h).flatten(1))
         semantic_embeddings = torch.stack(semantic_embeddings, dim=1)                  # (N, M, D * K)
 
         return self.semantic_attention(semantic_embeddings)                            # (N, D * K)
 
 class HAN(nn.Module):
-    def __init__(self, meta_paths, in_size, hidden_size, out_size, num_heads, dropout):
+    def __init__(self, num_meta_paths, in_size, hidden_size, out_size, num_heads, dropout):
         super(HAN, self).__init__()
 
         self.layers = nn.ModuleList()
-        self.layers.append(HANLayer(meta_paths, in_size, hidden_size, num_heads[0], dropout))
+        self.layers.append(HANLayer(num_meta_paths, in_size, hidden_size, num_heads[0], dropout))
         for l in range(1, len(num_heads)):
-            self.layers.append(HANLayer(meta_paths, hidden_size * num_heads[l-1],
+            self.layers.append(HANLayer(num_meta_paths, hidden_size * num_heads[l-1],
                                         hidden_size, num_heads[l], dropout))
         self.predict = nn.Linear(hidden_size * num_heads[-1], out_size)
 
